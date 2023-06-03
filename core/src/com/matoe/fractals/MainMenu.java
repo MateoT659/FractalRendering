9cebcc08eb3c3414ba94feb35b24274587695a62
Matoe <mateot659@gmail.com>
2023-06-03 18:24:25 -0400

    new display system

--- core/src/com/matoe/fractals/MainMenu.java
+++ core/src/com/matoe/fractals/MainMenu.java
@@ -1,27 +1,30 @@
 package com.matoe.fractals;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.utils.ScreenUtils;
 
+import java.awt.*;
+
 public class MainMenu implements Screen {
-    final int BLEN = 400;
-    final int DELTA = (FractalRenderer.WIDTH - BLEN*3)/4+20;
+
     FractalRenderer game;
     OrthographicCamera camera;
     Texture triangleImage;
     Texture fernImage;
     Texture dragonImage;
+    float SCALE;
 
     public MainMenu(FractalRenderer game){
         this.game = game;
         camera = new OrthographicCamera();
-        camera.setToOrtho(false, game.WIDTH,game.HEIGHT);
+        camera.setToOrtho(false, FractalRenderer.WIDTH,FractalRenderer.HEIGHT);
         triangleImage = new Texture("TriangleButton.png");
         fernImage = new Texture("FernButton.png");
         dragonImage = new Texture("DragonButton.png");
-
+        SCALE = (float) 400 / dragonImage.getHeight();
     }
     @Override
     public void show() {
@@ -32,11 +35,14 @@ public class MainMenu implements Screen {
     public void render(float delta) {
         ScreenUtils.clear(0,0,.05f,1);
         game.batch.begin();
-        game.batch.draw(dragonImage, DELTA,30, BLEN,BLEN);
-        game.batch.draw(triangleImage, (FractalRenderer.WIDTH-BLEN)/2,30, BLEN,BLEN);
-        game.batch.draw(fernImage,FractalRenderer.WIDTH-BLEN-DELTA,30, BLEN,BLEN);
+        game.batch.draw(dragonImage, FractalRenderer.WIDTH/5 - dragonImage.getWidth()*SCALE/2 -15,30, dragonImage.getWidth()*SCALE, dragonImage.getHeight()*SCALE);
+        game.batch.draw(triangleImage, FractalRenderer.WIDTH/2 - triangleImage.getWidth()*SCALE/2,30, triangleImage.getWidth()*SCALE, triangleImage.getHeight()*SCALE);
+        game.batch.draw(fernImage,4*FractalRenderer.WIDTH/5 - fernImage.getWidth()*SCALE/2 +15,30, fernImage.getWidth()*SCALE, fernImage.getHeight() * SCALE);
         game.batch.end();
+        Rectangle r = new Rectangle();
+        if(Gdx.input.isTouched()){
 
+        }
     }
 
     @Override
